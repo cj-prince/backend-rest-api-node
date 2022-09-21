0c171ec7b83a5ede13f467d23525a14f49b81723
cj-prince <davids.chijioke@gmail.com>
2022-09-21 14:04:06 +0100

    error handler and asynchander

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,25 @@
-const getGoals = (req, res) => {
+const asynchandler = require('express-async-handler')
+
+
+const getGoals = asynchandler(async (req, res) => {
   res.status(200).json({message:'Get goals'})
-}
+})
 
-const setGoal = (req, res) => {
+const setGoal = asynchandler(async (req, res) => {
+  if(!req.body.text){
+    res.status(400)
+    throw new Error('Text feild required')
+  }
   res.status(200).json({message:'Set goals'})
-}
+})
 
-const updateGoal = (req, res) => {
+const updateGoal = asynchandler(async (req, res) => {
   res.status(200).json({message:`Update goal ${req.params.id}`})
-}
+})
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asynchandler(async (req, res) => {
   res.status(200).json({message:`Delete goal ${req.params.id}`})
-}
+})
 
 
 
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,14 +2,8 @@ const express = require('express')
 const router = express.Router()
 const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
 
-
-router.get('/', getGoals)
-
-router.post('/', setGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 
 module.exports = router
